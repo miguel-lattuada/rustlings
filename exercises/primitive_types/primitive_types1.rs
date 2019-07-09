0ba72a5927eead40238ea6a3383646789d2d9e26
Miguel Lattuada <mlattuada@altimetrik.com>
2019-07-09 15:51:02 -0300

    Solved {primitive types} exercises

--- exercises/primitive_types/primitive_types1.rs
+++ exercises/primitive_types/primitive_types1.rs
@@ -6,11 +6,12 @@ fn main() {
     // Booleans (`bool`)
 
     let is_morning = true;
+    
     if is_morning {
         println!("Good morning!");
     }
 
-    let // Finish the rest of this line like the example! Or make it be false!
+    let is_evening = true; // Finish the rest of this line like the example! Or make it be false!
     if is_evening {
         println!("Good evening!");
     }
